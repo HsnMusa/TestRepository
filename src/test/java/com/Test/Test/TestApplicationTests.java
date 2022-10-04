363d1e79454ecf217182060618ea344e02a70168
hmoussa <Trade@KN1234>
2022-10-04 10:56:54 +0200

    readd test cases

--- src/test/java/com/Test/Test/TestApplicationTests.java
+++ src/test/java/com/Test/Test/TestApplicationTests.java
@@ -7,14 +7,14 @@ import java.math.BigDecimal;
 
 @SpringBootTest
 class TestApplicationTests {
-//	MainController controller = new MainController();
-//
-//
-//	@Test
-//	void contextLoads() throws Exception {
-//		BigDecimal multiplication = controller.multiply(new BigDecimal(5),new BigDecimal(1));
-//		if(multiplication.equals(new BigDecimal(0))) {
-//			throw new RuntimeException("result is zero");
-//		}
-//	}
+	MainController controller = new MainController();
+
+
+	@Test
+	void contextLoads() throws Exception {
+		BigDecimal multiplication = controller.multiply(new BigDecimal(5),new BigDecimal(1));
+		if(multiplication.equals(new BigDecimal(0))) {
+			throw new RuntimeException("result is zero");
+		}
+	}
 }
